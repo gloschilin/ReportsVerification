93d6ad465380c2f679b6966c751c90dfdd1f8a2a
Loschilin <gloschilin@gmail.com>
2017-11-02 20:11:09 +0300

    Валидация база + первые два соотношения

---
+++ Source/ReportsVerification.Web/Extentions/StringExtentions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReportsVerification.Web.Extentions
+{
+    public static class StringExtentions
+    {
+        public static decimal ToDecimal(this string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Source/ReportsVerification.Web/Services/Validation/IReportsValidationService.cs
+++ Source/ReportsVerification.Web/Services/Validation/IReportsValidationService.cs
@@ -1,10 +1,143 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using ReportsVerification.Web.DataObjects;
+using ReportsVerification.Web.DataObjects.Dates;
+using ReportsVerification.Web.DataObjects.ReportInfoObjects;
+using ReportsVerification.Web.DataObjects.Xsd.Nds;
+using ReportsVerification.Web.Extentions;
+using ReportsVerification.Web.Repositories.Interfaces;
 
 namespace ReportsVerification.Web.Services.Validation
 {
+    public enum ValidationStepType
+    {
+        Nds1Vosmechenie,
+        Nds1Deduction
+    }
+
     public interface IReportsValidationService
     {
-        void Validate(IEnumerable<Report> reports, SessionInfo sessionInfo);
+        void Validate(IReadOnlyCollection<Report> reports, SessionInfo sessionInfo);
+    }
+
+    public interface IReportsConcreteValidationService : IReportsValidationService
+    {
+
+    }
+
+    public class ReportsValidationService: IReportsValidationService
+    {
+        private readonly IEnumerable<IReportsConcreteValidationService> _services;
+
+        public ReportsValidationService(IEnumerable<IReportsConcreteValidationService> services)
+        {
+            _services = services;
+        }
+
+        public void Validate(IReadOnlyCollection<Report> reports, SessionInfo sessionInfo)
+        {
+            foreach (var service in _services)
+            {
+                service.Validate(reports, sessionInfo);
+            }
+        }
+    }
+
+    public interface IValidationHandler
+    {
+        void Wrong(ValidationStepType type);
+        void Success(ValidationStepType type);
+    }
+
+    public abstract class CommonConcreteValidationService : IReportsConcreteValidationService
+    {
+        private readonly IValidationHandler _handler;
+        protected abstract ValidationStepType Type { get; }
+
+        protected CommonConcreteValidationService(IValidationHandler handler)
+        {
+            _handler = handler;
+        }
+
+        public void Validate(IReadOnlyCollection<Report> reports, SessionInfo sessionInfo)
+        {
+            if (IsValid(reports, sessionInfo))
+            {
+                _handler.Success(Type);
+            }
+            else
+            {
+                _handler.Wrong(Type);
+            }
+        }
+
+        protected abstract bool IsValid(IReadOnlyCollection<Report> reports, SessionInfo sessionInfo);
+    }
+
+    /// <summary>
+    /// В декларации по НДС за 1 квартал заявлено возмещение. Готовьтесь к камеральной проверке.
+    /// Ссылки на статьи
+    /// </summary>
+    public class Nds1Vosmechenie: CommonConcreteValidationService
+    {
+        public Nds1Vosmechenie(IValidationHandler handler) : base(handler)
+        {
+        }
+
+        protected override ValidationStepType Type => ValidationStepType.Nds1Vosmechenie;
+        protected override bool IsValid(IReadOnlyCollection<Report> reports, SessionInfo sessionInfo)
+        {
+            var ndsReports = reports
+                .Where(e => e.ReportType == ReportTypes.Nds)
+                .Where(e => ((ReportInfoRevistion<DateOfQuarter>) e.GetReportInfo()).ReportPeriod.Quarter == 1)
+                .Select(e => e.XsdReport).OfType<Файл>().ToArray();
+
+            return !ndsReports.Any() || ndsReports.All(e => e.Документ.НДС.СумУплНП.СумПУ_1731.ToDecimal() == 0);
+        }
+    }
+
+    /// <summary>
+    /// В декларации по НДС за 1 квартал доля вычетов превышает безопасную 
+    /// (нужна ссылка на сайт, где есть таблица с долями).
+    /// </summary>
+    public class Nds1Deduction: CommonConcreteValidationService
+    {
+        private readonly ICatalogRepository _catalogRepository;
+
+        public Nds1Deduction(IValidationHandler handler, ICatalogRepository catalogRepository) : base(handler)
+        {
+            _catalogRepository = catalogRepository;
+        }
+
+        protected override ValidationStepType Type => ValidationStepType.Nds1Deduction;
+        protected override bool IsValid(IReadOnlyCollection<Report> reports, SessionInfo sessionInfo)
+        {
+            if (!sessionInfo.RegionId.HasValue)
+            {
+                return true;
+            }
+
+            foreach (var report in reports
+                .Where(e => e.ReportType == ReportTypes.Nds)
+                .Where(e => ((ReportInfoRevistion<DateOfQuarter>)e.GetReportInfo()).ReportPeriod.Quarter == 1)
+                .Where(e=>(e.XsdReport as Файл)?.Документ.НДС.СумУплНП.СумПУ_1731.ToDecimal() > 0).ToList())
+            {
+                var file = (Файл)report.XsdReport;
+                var deduction = _catalogRepository.GetDeduction(
+                    report.GetReportInfo().GetStartReportPeriod(),
+                    sessionInfo.RegionId.Value);
+
+                var incorrect = file.Документ.НДС.СумУпл164.СумНалВыч.НалВычОбщ.ToDecimal()
+                    /   file.Документ.НДС.СумУпл164.СумНалОб.СумНалВосст.СумНалВс.ToDecimal()*100 
+                    >= deduction.FirstQuaterAmount;
+
+                if (!incorrect)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
